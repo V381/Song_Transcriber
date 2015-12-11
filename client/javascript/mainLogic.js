590cac77e8fe9a04fcb06ccee81287ec19a2b5d0
Pavle <paavlee@yahoo.com>
2015-12-11 14:41:10 +0100

    Front end files
    Front end files

---
+++ client/javascript/mainLogic.js
@@ -0,0 +1,34 @@
+(function() {
+    var form = document.getElementsByTagName('form')[0];
+    var audio = document.querySelector('audio');
+
+
+    var audioTempoMap = {
+        '100%' : 1.0,
+        '75%' : 0.7,
+        '50%' : 0.5
+    };
+
+    $('ul li > button').each(function(val) {
+        $(this).bind('click', function () {
+            for(var i in audioTempoMap){
+                if(audioTempoMap.hasOwnProperty(i)){
+                    if(i === $(this).html()){
+                        $('.songTempo').html(i);
+                        audio.playbackRate = audioTempoMap[i];
+                    }
+                }
+            }
+        });
+    });
+
+    $.get('/getFile', function(data){
+        $('.songName').html('? ' + data.substr(7));
+    });
+
+    audio.addEventListener('play', function() {
+        $.get('/getFile', function(data){
+            audio.src = data;
+        })
+    });
+})();
